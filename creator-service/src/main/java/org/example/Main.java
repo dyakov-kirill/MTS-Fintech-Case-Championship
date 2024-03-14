73983cf4e3af0975dda0847b1582cae9876cfba0
Kirill Dyakov <kira-d03@mail.ru>
2024-03-14 15:45:51 +0300

    Initial commit

--- creator-service/src/main/java/org/example/Main.java
+++ creator-service/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
-package ru.mts;
+package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("h");
+        System.out.println("Hello world!");
     }
 }
